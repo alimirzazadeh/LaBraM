63cd2cdcc77def32db3ba2a43a6ab35559075a5c
alimirzazadeh <alimirzazadeham@gmail.com>
2025-12-29 17:27:35 -0500

    .

--- dataset_maker/make_TUEV.py
+++ dataset_maker/make_TUEV.py
@@ -49,11 +49,8 @@ def BuildEvents(signals, times, EventData, spec_true=None, spec_recon=None):
             :, offset + start - 2 * int(fs) : offset + end + 2 * int(fs)
         ]
         if spec_true is not None:
-            try:
-                features_spec_true[i, :, :, :] = spec_true[i, :, int((offset + start) // fs) - 2 : int((offset + end) // fs) + 2]
-                features_spec_recon[i, :, :, :] = spec_recon[i, :, int((offset + start) // fs) - 2 : int((offset + end) // fs) + 2]
-            except:
-                bp() 
+            features_spec_true[i, :, :, :] = spec_true[:, :, int((offset + start) // fs) - 2 : int((offset + end) // fs) + 2]
+            features_spec_recon[i, :, :, :] = spec_recon[:, :, int((offset + start) // fs) - 2 : int((offset + end) // fs) + 2]
         offending_channel[i, :] = int(chan)
         labels[i, :] = int(EventData[i, 3])
     if spec_true is not None:
