633efe469a74de00eeee7026ff431f345b7acf2a
alimirzazadeh <alimirzazadeham@gmail.com>
2025-12-30 13:44:07 -0500

    .

--- run_class_finetuning.py
+++ run_class_finetuning.py
@@ -144,7 +144,7 @@ def get_args():
                         help='resume from checkpoint')
     parser.add_argument('--auto_resume', action='store_true')
     parser.add_argument('--no_auto_resume', action='store_false', dest='auto_resume')
-    parser.set_defaults(auto_resume=True)
+    parser.set_defaults(auto_resume=False)
 
     parser.add_argument('--save_ckpt', action='store_true')
     parser.add_argument('--no_save_ckpt', action='store_false', dest='save_ckpt')
