4a23ae242a9a790b34c90fd27ade5c3e05148198
alimirzazadeh <alimirzazadeham@gmail.com>
2026-01-13 10:28:09 -0500

    .

---
+++ check_regenerations.py
@@ -0,0 +1,55 @@
+from tqdm import tqdm
+import os
+import pickle
+from ipdb import set_trace as bp
+
+root = "/data/netmit/sleep_lab/EEG_FM/TUAB/data/v3.0.1/edf/processed/test_with_spec"
+files = os.listdir(root)
+unsuccessful_files = []
+successful_files = []
+for file in tqdm(files):
+    if file.endswith('.pkl'):
+        with open(os.path.join(root, file), 'rb') as f:
+            sample = pickle.load(f)
+            if 'spec_true_bw1' not in sample:
+                unsuccessful_files.append(file)
+            else:
+                successful_files.append(file)
+
+print(f'Number of unsuccessful files: {len(unsuccessful_files)}')
+print(f'Number of successful files: {len(successful_files)}')
+bp() 
+
+root = "/data/netmit/sleep_lab/EEG_FM/TUAB/data/v3.0.1/edf/processed/train_with_spec"
+files = os.listdir(root)
+unsuccessful_files = []
+successful_files = []
+for file in tqdm(files):
+    if file.endswith('.pkl'):
+        with open(os.path.join(root, file), 'rb') as f:
+            sample = pickle.load(f)
+            if 'spec_true_bw1' not in sample:
+                unsuccessful_files.append(file)
+            else:
+                successful_files.append(file)
+
+print(f'Number of unsuccessful files: {len(unsuccessful_files)}')
+print(f'Number of successful files: {len(successful_files)}')
+bp() 
+
+root = "/data/netmit/sleep_lab/EEG_FM/TUAB/data/v3.0.1/edf/processed/val_with_spec"
+files = os.listdir(root)
+unsuccessful_files = []
+successful_files = []
+for file in tqdm(files):
+    if file.endswith('.pkl'):
+        with open(os.path.join(root, file), 'rb') as f:
+            sample = pickle.load(f)
+            if 'spec_true_bw1' not in sample:
+                unsuccessful_files.append(file)
+            else:
+                successful_files.append(file)
+
+print(f'Number of unsuccessful files: {len(unsuccessful_files)}')
+print(f'Number of successful files: {len(successful_files)}')
+bp() 
